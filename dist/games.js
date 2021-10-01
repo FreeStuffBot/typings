ca744502d7aa083091de4605f11fae7d5eb8fd53
maanex <andreas@maanex.me>
2021-10-01 15:36:54 +0200

    update format

--- dist/games.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GameFlag = void 0;
-var GameFlag;
-(function (GameFlag) {
-    GameFlag[GameFlag["TRASH"] = 1] = "TRASH";
-    GameFlag[GameFlag["THIRDPARTY"] = 2] = "THIRDPARTY";
-})(GameFlag = exports.GameFlag || (exports.GameFlag = {}));
-//# sourceMappingURL=games.js.map
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './games';
-export * from './guilds';
-export * from './structs';
